2e5eb13eb44107d9ed0cb36c0e3c27d0353c0e45
ZTF <3061587471@qq.com>
2024-10-13 23:41:04 +0800

    提交代码

--- src/test/java/com/itheima/test/UserMapperTest.java
+++ src/test/java/com/itheima/test/UserMapperTest.java
@@ -23,16 +23,15 @@ public class UserMapperTest {
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
 
+
+
+
         //2.获取sqlSession对象
         SqlSession sqlSession = sqlSessionFactory.openSession();
-
         //3.1 获取UserMapper接口的代理对象，可以不再需要在UserMapper里多次查找sql语句的id
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
         User user = userMapper.selectById(id);
-
         System.out.println(user);
-
         sqlSession.close();
     }
-
 }
